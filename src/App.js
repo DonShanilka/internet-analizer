3be2ad314dcb8973f9a5ffd039ecb842e81aea90
Shanilka <shanilka799@gmail.com>
2024-07-20 11:53:57 +0530

    add funtion

--- src/App.js
+++ src/App.js
@@ -3,15 +3,53 @@ import { useState } from 'react';
 import { Layout, Typography, Button, Col, Row, Image, Statistic, Card, message } from "antd";
 import { BarcodeOutlined, ArrowUpOutlined, ArrowDownOutlined, HomeOutlined, FieldTimeOutlined, RedoOutlined } from "@ant-design/icons";
 import axios from 'axios';
-import speed from "./images/"
+import speed from "./images/performance.png";
 
 
 const { Content } = Layout;
+const { Title } = Typography;
+
 function App() {
+  //hook
+  const [loading, setLoading] = useState(false);
+  const [btnStatus, setBtnStatus] = useState(0);
+  const [data, setData] = useState({
+    downloadSpeed: 0,
+    uploadSpeed: 0,
+    dowmloaded: 0,
+    latency: 0,
+    bufferBloat: 0,
+    userLocation: " - ",
+    userIp: "0.0.0.0",
+  });
+
+  //headres
+  const config = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    Accept: "application/json",
+  };
+
+  //Button submit arrow Function
+  const onSubmit = () => {
+    setLoading(true);
+    setBtnStatus(1);
+
+    axios.post("http://localhost:4000/speed", config).then((val) => {
+      setLoading(false);
+      setData(val?.data?.data);
+    }).catch((e) => {
+      setLoading(false)
+      message.error("Somthing went Wrong")
+    });
+  }
+
   return (
     <>
-      <Layout style={{ padding: "0 24px 24px", marginTop: 100, height: "100vh" }}>
-        <Content></Content>
+      <Layout>
+        <Layout style={{ padding: "0 24px 24px", marginTop: 100, height: "100vh" }}>
+          <Content></Content>
+        </Layout>
       </Layout>
     </>
   );
